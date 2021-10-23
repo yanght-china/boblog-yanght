c74806f34297d8f5c8c18e7ed46d81a8a05e215f
杨洪涛 <422209678@qq.com>
2021-10-23 13:27:41 +0800

    报错返回模板

--- core/http-exception.js
+++ core/http-exception.js
@@ -7,3 +7,56 @@ class HttpException extends Error {
     }
 }
 
+class ParameterException extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.code = 400
+        this.msg = msg || '参数错误'
+        this.errorCode = errorCode || 10000
+    }
+}
+
+class AuthFailed extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.code = 401
+        this.msg = msg || '授权失败'
+        this.errorCode = errorCode || 10004
+    }
+}
+
+class NotFound extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.code = 404
+        this.msg = msg || '404找不到'
+        this.errorCode = errorCode || 10005
+    }
+}
+
+class Forbidden extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.code = 403
+        this.msg = msg || '禁止访问'
+        this.errorCode = errorCode || 10006
+    }
+}
+
+class Existing extends HttpException {
+    constructor(msg, errorCode) {
+        super()
+        this.code = 412
+        this.msg = msg || '已存在'
+        this.errorCode = errorCode || 10006
+    }
+}
+
+module.exports = {
+    HttpException,
+    ParameterException,
+    AuthFailed,
+    NotFound,
+    Forbidden,
+    Existing
+}
